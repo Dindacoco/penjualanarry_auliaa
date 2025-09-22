8470d1d997ef2caa24a80a4a238b95b2271dfe10
dindacoco <Dindaulia006@gmail.com>
2025-09-22 20:30:56 +0700

    project polganamart

---
+++ polganmart.php
@@ -0,0 +1,8 @@
+<?php
+// Commit 1 - Setup awal
+$nama_barang = ["milkku", "beng-beng", "popmie", "coca-cola","waffle"];
+$harga_barang = [3000, 1000, 5000, 5000, 2000];
+
+echo "--POLGAN MART--<br>";
+echo "Daftar Pembelian<br><br>";
+
