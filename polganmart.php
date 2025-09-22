4c899fa2d7b0490e1b49b943e5adbc59d2e8e82c
dindacoco <Dindaulia006@gmail.com>
2025-09-22 20:46:17 +0700

    logika pembelian

--- polganmart.php
+++ polganmart.php
@@ -6,3 +6,18 @@ $harga_barang = [3000, 1000, 5000, 5000, 2000];
 echo "--POLGAN MART--<br>";
 echo "Daftar Pembelian<br><br>";
 
+// Commit 2 - Logika pembelian
+$beli = [5];
+$jumlah = [1];
+$total = [7];
+$grandtotal = 0;
+
+// pilih barang dan jumlah secara random
+for ($i = 0; $i < rand(1,5
+); $i++) {
+    $beli[$i] = $nama_barang[$i];
+    $jumlah[$i] = rand(1, 5); // jumlah random 1-5
+    $total[$i] = $harga_barang[$i] * $jumlah[$i];
+    $grandtotal += $total[$i];
+}
+
